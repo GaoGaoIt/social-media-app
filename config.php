4e213b5ffb0ec350a5c59a5812513d6894225684
sam <samchan952@gmail.com>
2023-12-11 16:39:51 +0800

    changes config database

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 
 $db_host = "localhost";
 $db_user = "root";
-$db_password = "";
-$db_name = "student_social_media";
+$db_password = "886488";
+$db_name = "eventswave";
 
 $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
 
