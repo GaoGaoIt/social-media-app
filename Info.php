2b3c1add50236f62c6b5da7d285291a35ec40136
sam <samchan952@gmail.com>
2023-12-15 09:44:51 +0800

    info page header change

--- Info.php
+++ Info.php
@@ -39,29 +39,9 @@
 
 <!-- Nav Bar Design -->
 
-<nav class="navbar">
-
-  <div class="nav-wrapper">
-
-    <img src="assets/images/black_logo.png" class="brand-img" id="logo-img">
-
-    <div class="nav-items">
-
-        <a href="Events.php" style="text-decoration: none; color: #1c1f23"><i class="icon fas fa-flag fa-lg"></i></a>
-
-        <a href="shorts.php" style="text-decoration: none; color: #1c1f23"><i class="icon fas fa-video fa-lg"></i></a>
-
-        <div class="icon user-profile">
-
-            <a href="my_Profile.php" ><i class="fas fa-user-circle fa-lg"></i></a>
-
-        </div>
-    </div>
-
-  </div>
-
-</nav>
-
+<?php
+    require 'component/header.php';
+?>
 <!-- design photo gallery -->
 
 
