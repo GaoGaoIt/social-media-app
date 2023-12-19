06e7c40880edb1a1a5486994e26add42503c92eb
sam <samchan952@gmail.com>
2023-12-19 14:46:30 +0800

    test

--- Single-Video.php
+++ Single-Video.php
@@ -440,7 +440,7 @@ session_regenerate_id(true);
                                         <textarea class="form-control" id="message-text" maxlength="500" name="caption"><?php echo $post['Caption']; ?></textarea>
                                     </div>
 
-                                    <input type="hidden" name="post_id" value="<?php echo $post['Video_ID']; ?>">
+                                    <input type="hidden" name="post_id" value="<?php echo $post['content_id']; ?>">
                                     <button type="submit" class="btn btn-outline-primary" name="edit">Edit Post</button>
                                 </form>
                             </div>
