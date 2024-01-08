8164656b5a894e9654f52e28be2d8a3a2a0f2435
sam <samchan952@gmail.com>
2024-01-08 10:47:06 +0800

    chanage titlink header to index

--- Admin/studentPostPage.php
+++ Admin/studentPostPage.php
@@ -66,7 +66,7 @@ $result = fetchPostsData();
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="index3.html" class="brand-link">
+      <a href="index.php" class="brand-link">
         <img src="../assets/images/profiles/synergyCollegeDefault.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-2" style="opacity: .8">
         <span class="brand-text font-weight-light">Synergy College</span>
       </a>
