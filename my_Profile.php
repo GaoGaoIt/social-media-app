3e81eaa9b543f3a60b4fb4ef8483a97a24a87d27
sam <samchan952@gmail.com>
2024-01-11 08:51:04 +0800

    test

--- my_Profile.php
+++ my_Profile.php
@@ -235,7 +235,7 @@
                                             <li class="gallery-items-likes"><span class="hide-gallery-elements">Reactions : 
                                             </span>
                                                 :id <?php echo $post['content_id'] ?>
-                                                :likes count <?php PostLikesCount() ?>
+                                                :likes count <?php echo PostLikesCount() ?>
                                                 <i class="icon fas fa-thumbs-up"></i>
                                             </li>
 
